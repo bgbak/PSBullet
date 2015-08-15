2fed32989306dcafa6c67359b37ebf0152b36d3e
Bjørn Gustav Baklid <bg@dyret.net>
2015-08-15 13:22:21 +0200

    Corrected typo. Cmdlets now load

--- PSBullet/PSBullet/PSBullet.psm1
+++ PSBullet/PSBullet/PSBullet.psm1
@@ -1,5 +1,5 @@
 # Import the individual files
 . (Join-Path $PSScriptRoot '.\GetApiKey.ps1')
-. (Join-Path $PSScriptRoot '.\GetPusbulletDevices.ps1')
+. (Join-Path $PSScriptRoot '.\GetPushbulletDevices.ps1')
 . (Join-Path $PSScriptRoot '.\SendPushbulletMessage.ps1')
 
