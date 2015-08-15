cf16dfdf4f1cb7eaee93a9a009afc66b55c1c480
Bjørn Gustav Baklid <bg@dyret.net>
2015-08-15 12:57:59 +0200

    Updated the main module

--- PSBullet/PSBullet/PSBullet.psm1
+++ PSBullet/PSBullet/PSBullet.psm1
@@ -1,6 +1,5 @@
-<#
-	My Function
-#>
-function Get-Function {
+# Import the individual files
+. (Join-Path $PSScriptRoot '.\GetApiKey.ps1')
+. (Join-Path $PSScriptRoot '.\GetPusbulletDevices.ps1')
+. (Join-Path $PSScriptRoot '.\SendPushbulletMessage.ps1')
 
-}
