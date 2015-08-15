66dc644ce12da391a62de867b60f3b39413d2dbb
Bjørn Gustav Baklid <bg@dyret.net>
2015-08-15 12:58:21 +0200

    Exporting the function

--- PSBullet/PSBullet/GetPushbulletDevices.ps1
+++ PSBullet/PSBullet/GetPushbulletDevices.ps1
@@ -1,46 +1,41 @@
 function Get-PushBulletDevices {
-<#
-	.SYNOPSIS
-    Get a list of devices asociated with your pushbullet account.
-    Enter your apikey in a file named pusbhullet.cfg, and place it in the same folder as this file.
-	.EXAMPLE
-    Get-PushBulletDevices
-	.OUTPUT
-	Returns a PSCustomObject with your devices.
-  #>
-[CmdletBinding()]
-param ()
-begin {
-	# Get the apikey
-	$apikey = Get-PusbhulletApiKey
-	Write-Verbose "Successfully got apikey"
-    if($apikey -eq $null){
-		Write-Error -Message "Could not read api key" -Category ReadError
+	<#
+		.SYNOPSIS
+		Get a list of devices asociated with your pushbullet account.
+		Enter your apikey in a file named pusbhullet.cfg, and place it in the same folder as this file.
+		.EXAMPLE
+		Get-PushBulletDevices
+		.OUTPUT
+		Returns a PSCustomObject with your devices.
+	  #>
+	[CmdletBinding()]
+	param ()
+	begin {
+		# Get the apikey
+		$apikey = Get-PusbhulletApiKey
+		Write-Verbose "Successfully got apikey"
+		if($apikey -eq $null){
+			Write-Error -Message "Could not read api key" -Category ReadError
+		}
+		else{
+			# Set the apikey in the headers.
+			$headers = @{Authorization = "Bearer $apikey"}
+		}
 	}
-	else{
-		# Set the apikey in the headers.
-		$headers = @{Authorization = "Bearer $apikey"}
-	}
-}
 
-process{
-    if ($apikey -eq $null){
-		Write-Error "Api key not found" -Category ReadError
-		Exit 1
-	}
-	else{
+	process{
 		write-verbose "Getting devices"
-		$Requestattempt = Invoke-WebRequest -Uri https://api.pushbullet.com/v2/devices -Method Get  -Credential $cred
-		Write-Debug "Requestattempt: $Requestattempt"
+		$Requestattempt = Invoke-WebRequest -Uri https://api.pushbullet.com/v2/devices -Method Get  -Headers $headers
 		If ($Requestattempt.StatusCode -eq "200"){
 			Write-Verbose "Got devices successfully"
 			($Devices = $Requestattempt.Content|Convertfrom-json).Devices
 			Write-Verbose "Request returned $($Devices.Count) devices"
-			Return $Devices
+			Return $Devices.devices
 		}
 		else {
 			Write-Error -Message "Error: Something went wrong. Check `$attempt for info" -Category WriteError
 			$global:attempt = $Requestattempt
 		}
-    }
+	}
 }
+Export-ModuleMember -Function Get-PushBulletDevices
