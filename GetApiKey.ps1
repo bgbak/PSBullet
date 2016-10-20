85cbe36e8c0c6e808320ad2fe28501a792397716
Bjørn Gustav Baklid <bg@dyret.net>
2016-10-20 15:03:58 +0200

    Moved location of api key to user registry

--- GetApiKey.ps1
+++ GetApiKey.ps1
@@ -1,29 +1,15 @@
-function Get-PusbhulletApiKey{	
+function Get-PushBulletApiKey{	
 <#
 	.SYNOPSIS
-	Get apikey from file
-	.DESCRIPTION
-	Gets the apikey from the specified path.
-	If no path is specified, the file is assumed to be in the script root in a file named apikey.txt
+	Get the api key from registry
 	.EXAMPLE
-	Get-PushbulletApiKey
-	.EXAMPLE
-    Get-PushbulletApiKey -Path "C:\Pusbullet\apikey.cfg"
-	.PARAMETER Path
-	The path to the file with the apikey
+    $ApiKey = Get-PushbulletApiKey
+	
 #>	
-	param(
-		[string]$Path = (Join-Path -Path $PSSCriptroot -ChildPath apikey.txt)
-	)
-	Write-Verbose "Fetching api key from file"
-	try {
-		$apikey = Get-Content $Path
-		Write-Verbose "PSScriptRoot: $PSSCriptroot"
-		Write-Verbose "Got api key: $apikey"
-		Return $apikey	
-		}
+	if(!(Test-Path -Path HKCU:\software\PSBullet))
+        {Write-Error "Path to PSBullet API key not found in registry. Try to save it with Set-PushBulletApiKey" -Category ReadError
+        break}
 
-	catch {Write-Output "Could not get apikey from config file. Exiting now."
-	Return $Null
-		}
+    $ApiKey = (Get-ItemProperty -Path HKCU:\SOFTWARE\PSBullet -Name ApiKey).ApiKey
+    
 }
---
+++ SetPushbulletApiKey.ps1
@@ -0,0 +1,27 @@
+﻿function Set-PushBulletApiKey{	
+<#
+	.SYNOPSIS
+	Stores the API Key in registry under the users hive.
+	.DESCRIPTION
+	Stores the API key in the users hive to get around having it in a text file along with the module
+	.EXAMPLE
+	Set-PushbulletApiKey -ApiKey 
+	.PARAMETER Path
+	The API Key to store
+#>	
+	param(
+		[Parameter(Mandatory=$True,Position=1)][string]$ApiKey
+	)
+	# Test if path exists. if not, create
+    if(!(Test-Path HKCU:\Software\PSBullet))
+        {
+        Write-Debug "Registry path not found. Creating..."
+        New-Item -Path HKCU:\SOFTWARE\PSBullet
+        }
+
+    # Store the actual key
+    Set-ItemProperty -Path HKCU:\SOFTWARE\PSBullet -Name ApiKey -Value $ApiKey
+
+    # Test if key is stored
+    #Get-ItemProperty -Path HKCU:\SOFTWARE\PSBullet -Name ApiKey
+}
